e61fd1212515a483147465d16b74ada2c77a5342
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-12-26 13:46:36 +0000

    Update CanOpenBus.c

--- coMgt/CanOpenBus.c
+++ coMgt/CanOpenBus.c
@@ -1,5 +1,12 @@
 #include "coMgt/CanOpenBus.h"
 
+CoStatus CanOpenBus_setup(Uint busId)
+{
+  CoStatus result = E_COSTATUS_OK;
+
+  return result;
+}
+
 PUBLIC void CanOpenBus_storeItFromStart(Uint busId, Uint itSlotFromStart)
 {
 
