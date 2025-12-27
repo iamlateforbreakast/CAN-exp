d4a9846ad4840579a25fc9218bf7a75f0672391d
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-12-27 11:11:27 +0000

    Add coEvtDecode function to handle events

--- coMgt/CanOpenAction.c
+++ coMgt/CanOpenAction.c
@@ -29,3 +29,56 @@ PUBLIC void CanOpenAction_updateHkArea(CoCtx *pCtx, CoState entryState)
 {
 
 }
+
+PRIVATE CoEvt coEvtDecode (CoCtx *pCtx)
+{
+  CoEvt coEvt;
+  Uint idx;
+  Uint cmd;
+  
+  /* upon CYCLE event */
+  if ((pCtx->slotInCycle == 0) && (pCtx->itInCycle == 0))
+  {
+    /* upon a Cycle event, get input mstCmd if we are not in INIT */
+    if ((pCtx->mstCmdIn > pCtx->mstCmdOut) && (pCtx->curState > E_COSTATE_INIT))
+    {
+      idx = pCtx->mstCmdOut%CO_MST_CMD_QUEUE_LENGTH;
+      cmd = pCtx->mstCmdQueue[idx];
+      pCtx->mstCmdOut++;
+      coEvt = coEvtFromNmtCs(cmd);
+    }
+    /* check toggle timeout if we are in PRE_OP or OP mode */
+    else if ((pCtx->ttoggleTimer < 0) && 
+        ((pCtx->curState == E_COSTATE_PRE_OP)||(pCtx->curState == E_COSTATE_OP)))
+    {
+      coEvt = E_COEVT_TT_TIMEOUT;
+    }
+    /* check treset timeout if we are in OP mode */
+    else if ((pCtx->tresetTimer < 0) && 
+             ((pCtx->curState == E_COSTATE_PRE_OP)||(pCtx->curState == E_COSTATE_OP)))
+    {
+      if(pCtx->curState == E_COSTATE_OP)
+      {
+        pCtx->tresetTriggeredInOp = TRUE;
+      }
+      coEvt = E_COEVT_RESET;
+    }
+    else if((pCtx->tresetTriggeredInOp == TRUE) && 
+            (pCtx->curState == E_COSTATE_PRE_OP))
+    {
+      pCtx->tresetTriggeredInOp = FALSE;
+      coEvt = E_COEVT_START;
+    }
+    else /* otherwise, simple cycle event */
+    {
+      coEvt = E_COEVT_10HZ;
+    }
+  }
+  /* upon slot event */
+  else
+  {
+    coEvt = E_COEVT_200HZ;
+  }
+  
+  return coEvt;
+}
