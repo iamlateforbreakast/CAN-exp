aee2f52a94772f4acb8acf8c451908b5bed0dc00
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-12-27 11:09:55 +0000

    Implement CanOpenAction_busMgrAutom function
    Added CanOpenAction_busMgrAutom function for bus management.

--- coMgt/CanOpenAction.c
+++ coMgt/CanOpenAction.c
@@ -18,6 +18,13 @@ PUBLIC CoStatus CanOpenAction_setup(CoCtx *pCtx, U16 evtPid)
   return E_COSTATUS_OK;
 }
 
+void CanOpenAction_busMgrAutom(CoCtx *pCtx)
+{
+  CoEvt evt = E_COEVT_END;
+  CoState state = E_COSTATE_END;
+  CoTrans const *pTrans = NULL;
+}
+
 PUBLIC void CanOpenAction_updateHkArea(CoCtx *pCtx, CoState entryState)
 {
 
